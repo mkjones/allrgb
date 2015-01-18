e9340104ccfa7950f245fe66272c7cd4d0e7dae0
Matt Jones <mkjones@cs.stanford.edu>
2015-01-18 15:08:49 -0800

    move initialization logic into Filler class; fix some bugs with odd # of bits

--- all_colours.py
+++ all_colours.py
@@ -14,7 +14,7 @@ import filler
 # how many bits should we use per channel?  "normal" 24-bit color
 # uses 8 bits per channel, but will take a very long time to generate
 # a full image.  Image size = # colors = 2^(3 * bits)
-bits = 4
+bits = 3
 
 # How many pixels do we start by randomly filling in?  This is only
 # relevant for the "find the best place for a color" approach, rather
@@ -23,40 +23,13 @@ starting_pixels = 3
 
 # This is just a seed for the random number generator so we can do
 # things like performance testing deterministically
-seed = 27
+seed = 29
 
 print 'initializing'
 
 random.seed(seed)
-colorset = Colorset(bits)
-# This is technically only correct for an even number of bits, but
-# it rounds down for an odd number of bits, and there are simply some colors
-# that we never use.  Shrug.
-height = width = int(math.sqrt(colorset.size()))
-canvas = Canvas(width, height)
 
-# Grab some random starting colors just by making a randomly-sorted
-# list of all the colors and taking the first few.
-# Not the most efficient, but doesn't really matter.
-colors = [x for x in colorset.iterate()]
-random.shuffle(colors)
-
-# Choose random starting colors and place them randomly on the canvas
-last_y = 0
-last_x = 0
-for i in xrange(width/10):
-    starting_color = colorset.get_nearest(colors[i])
-    last_x = i * 10 # TODO why not randomize this?
-    canvas.set(last_x, last_y, starting_color)
-
-start_time = time.time()
-last_save_time = time.time()
-time_color = 0.0
-time_point = 0.0
-
-
-print ('last_x before', last_x)
-filler = filler.ByColorFiller(canvas, colorset, starting_pixels)
-#filler = filler.ByWalkFiller(canvas, colorset, starting_pixels)
+filler = filler.ByColorFiller(bits, starting_pixels)
+#filler = filler.ByWalkFiller(bits, starting_pixels)
 filler.go()
 
--- filler.py
+++ filler.py
@@ -1,22 +1,50 @@
 import time
+import math
+import random
+
 from canvas import Canvas
 from colorset import Colorset
 
 class Filler(object):
-    def __init__(self, canvas, colorset, starting_pixel_count):
-        self.canvas = canvas
-        self.colorset = colorset
-        self.starting_pixel_count = starting_pixel_count
+    def __init__(self, bits, starting_pixel_count):
+        self.colorset = Colorset(bits)
 
+        # This is technically only correct for an even number of bits, but
+        # it rounds down for an odd number of bits, and there are simply some colors
+        # that we never use.  Shrug.
+
+        height = width = int(math.sqrt(self.colorset.size()))
+        self.canvas = Canvas(width, height)
+
+        self.starting_pixel_count = starting_pixel_count
+        self.add_starting_pixels()
         self.start_time = time.time()
         self.last_save_time = time.time()
 
         self.time_color = 0.0
         self.time_point = 0.0
 
+    def add_starting_pixels(self):
+        # Grab some random starting colors just by making a randomly-sorted
+        # list of all the colors and taking the first few.
+        # Not the most efficient, but doesn't really matter.
+        colors = [x for x in self.colorset.iterate()]
+        random.shuffle(colors)
+        height = width = self.canvas.height
+        self.starting_pixel_list = []
+        for i in xrange(self.starting_pixel_count):
+            starting_color = self.colorset.get_nearest(colors[i])
+            x = random.randint(0, width-1)
+            y = random.randint(0, height-1)
+            self.starting_pixel_list.append((x, y))
+            self.canvas.set(x, y, starting_color)
+
     def write_image(self, i):
         bits = self.colorset.bits
-        name = '/tmp/colors-coloriter.%d.%d.%d.png' % (bits, self.starting_pixel_count, i)
+        name = '/tmp/colors-%s.%d.%d.%d.png' % (self.__class__.__name__,
+                                                bits,
+                                                self.starting_pixel_count,
+                                                i)
         avg_rate = i / (time.time() - self.start_time)
         print (name,
                time.time() - self.last_save_time,
@@ -52,15 +80,16 @@ class ByColorFiller(Filler):
 class ByWalkFiller(Filler):
 
     def go(self):
-        last_x = 0
-        last_y = 0
+        (last_x, last_y) = self.starting_pixel_list[-1]
         i = 0
-        print ('last_x after', last_x)
         while self.colorset.size() > 0:
             get_nearby_start = time.time()
 
             # find the open pixel nearest the last one we filled in
-            (x, y) = self.canvas.find_blank_nearby_opt(last_x, last_y)
+            nearby = self.canvas.find_blank_nearby_opt(last_x, last_y)
+            if nearby is None:
+                break
+            (x, y) = nearby
             diff = time.time() - get_nearby_start
             self.time_point += diff
 
